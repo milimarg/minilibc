d8426595e8e0b5a39a60ac98402512f596061e5b
Florent GUITTRE <florent.guittre@epitech.eu>
2023-10-13 22:04:09 +0100

    feat: add memcpy function for ARMv8-A 32 bits

---
+++ bonus/src/memcpy.s
@@ -0,0 +1,24 @@
+@ void *my_memcpy(void *dest, const void *src, size_t n);
+@ r0 my_memcpy(r0, r1, r2); (rdi, rsi, rdx)
+
+.global my_memcpy
+
+my_memcpy:
+    MOV r3, r0 @ The initial pointer is saved
+	B .loop @ Jump to the loop
+
+.loop:
+    CMP r2, #0 @ Check if there are no characters to copy
+    BEQ .stop @ If yes, break out of the loop
+    LDRB r4, [r1]
+    STRB r4, [r0]
+    @MOV al, BYTE [rsi] @ Copy byte of current value pointer into al register, to get the value
+    @MOV BYTE [rdi], al @ Copy value into rdi register, to get its pointer and put it in the destination slot
+    ADD r0, r0, #1 @ Get to the next destination pointer address
+    ADD r1, r1, #1 @ Get to the next source pointer address
+    SUB r2, r2, #1 @ Decrement rdx to get the remaining number of values to copy
+    B .loop @ Repeat the loop
+
+.stop:
+    MOV r0, r3 @ return the earlier backed up pointer
+    BX lr
